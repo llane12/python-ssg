7c1150c6bf70e1e914022a9e04a48ea8e85dee5a
llane12 <lukelane1@gmail.com>
2024-11-06 16:16:04 +0900

    Added HTMLNode and unit tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_raises(self):
+        node = HTMLNode()
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def test_repr(self):
+        node = HTMLNode()
+        self.assertEqual("HTMLNode(None, None, None, None)", repr(node))
+
+    def test_props_to_html_none(self):
+        node = HTMLNode(props=None)
+        self.assertEqual('', node.props_to_html())
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode(props={})
+        self.assertEqual('', node.props_to_html())
+
+    def test_props_to_html_values(self):
+        props = { "href": "/page.html", "target": "_blank" }
+        node = HTMLNode(props=props)
+        self.assertEqual(' href="/page.html" target="_blank"', node.props_to_html())
+    
