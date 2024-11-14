990ea93871b667a5d4da770a7e8ee1656086a041
llane12 <lukelane1@gmail.com>
2024-11-14 12:11:12 +0900

    Added Markdown block type identification

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     lines = markdown.split("\n")
     
@@ -15,3 +28,40 @@ def markdown_to_blocks(markdown):
     if len(temp) > 0:
         blocks.append(temp)
     return blocks
+
+def block_to_block_type(block):
+    matches = re.findall(r"(^#{1,6} )(.*)", block)
+    if len(matches) == 1 and len(matches[0]) == 2:
+        return BlockType.HEADING
+    matches = re.findall(r"(^`{3})([\s\S]*)(`{3}$)", block)
+    if len(matches) == 1 and len(matches[0]) == 3:
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+    block_types = set()
+    li_num = 1
+    for line in lines:
+        if len(line) < 1:
+            continue
+        if line[0] == ">":
+            block_types.add(BlockType.QUOTE)
+            continue
+
+        matches = re.findall(r"(^[\*-] )(.*)", line)
+        if len(matches) == 1 and len(matches[0]) == 2:
+            block_types.add(BlockType.UNORDERED_LIST)
+            continue
+
+        matches = re.findall(r"(^\d+\. )(.*)", line)
+        if len(matches) == 1 and len(matches[0]) == 2:
+            li_str = str(li_num)
+            if line[:len(li_str)] == li_str:
+                block_types.add(BlockType.ORDERED_LIST)
+                li_num += 1
+                continue
+        
+        block_types.add(BlockType.PARAGRAPH)
+    if len(block_types) == 1:
+        return block_types.pop()
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -3,7 +3,7 @@ from markdown_parser import *
 from textnode import *
 
 
-class TestMarkdownParser(unittest.TestCase):
+class TestMarkdownToBlocks(unittest.TestCase):
     def test_one_block_whitespace(self):
         markdown = "\n   This is normal text with leading and trailing whitespace    \n\n"
         blocks = markdown_to_blocks(markdown)
@@ -29,3 +29,155 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 """* This is the first list item in a list block\n* This is a list item\n* This is another list item"""
             ],
             blocks)
+
+class TestBlockToBlockType(unittest.TestCase):
+    # Input should be stripped of whitespace
+    def test_paragraph_type(self):
+        block = "This is normal text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_valid_heading_one(self):
+        block = "# Heading 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_valid_heading_three(self):
+        block = "### Heading 3"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_valid_heading_six(self):
+        block = "###### Heading 6"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_invalid_heading_one(self):
+        block = "#Heading 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_heading_seven(self):
+        block = "####### Heading 7"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_valid_code_one_line(self):
+        block = "```some code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.CODE, block_type)
+
+    def test_valid_code_three_lines(self):
+        block = "```some code\nmore code\nlast code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.CODE, block_type)
+
+    def test_invalid_code_start(self):
+        block = "``some code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_code_end(self):
+        block = "```some code` ``"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_valid_quote_one_line(self):
+        block = "> A quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_valid_quote_two_lines(self):
+        block = "> A quote\n>Over two lines" # No space required after >
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_invalid_quote_two_lines(self):
+        block = "> A quote\nOver two lines"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_valid_unordered_list_one_item(self):
+        block = "* Item 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.UNORDERED_LIST, block_type)
+
+    def test_valid_unordered_list_three_items(self):
+        block = "* Item 1\n- Item 2\n* Item 3"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.UNORDERED_LIST, block_type)
+
+    def test_invalid_unordered_list_one_item(self):
+        block = "*Item 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_unordered_list_one_item(self):
+        block = "-Item 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_unordered_list_three_items(self):
+        block = "* Item 1\n-Item 2\n* Item 3"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_valid_ordered_list_one_item(self):
+        block = "1. Item one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ORDERED_LIST, block_type)
+
+    def test_valid_ordered_list_three_items(self):
+        block = "1. Item one\n2. Item two\n3. Item three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ORDERED_LIST, block_type)
+
+    def test_invalid_ordered_list_one_item_missing_space(self):
+        block = "1.Item one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_ordered_list_one_item_wrong_character(self):
+        block = "1,Item one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_ordered_list_one_item_no_period(self):
+        block = "1 Item one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_ordered_list_one_item_start_at_zero(self):
+        block = "0. Item one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_ordered_list_three_items_missing_space(self):
+        block = "1. Item one\n2. Item two\n3.Item three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_ordered_list_three_items_numbering(self):
+        block = "1. Item one\n2. Item two\n2. Item three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_mixed_block_one(self):
+        block = "> Some code\n> More code\n- List item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_mixed_block_two(self):
+        block = "- List item\n* List item\n1. Ordered list item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_mixed_block_three(self):
+        block = "1. Item one\n2. Item two\n> Some code"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_invalid_mixed_block_four(self):
+        block = "1. Item one\n2. Item two\n* Item three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
